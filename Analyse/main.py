1cb4f1890273d011393beaaf6d9dafffe7ad6619
Ben <benjamin.boulet@etu.univ-lyon1.fr>
2023-04-07 15:20:42 +0200

    masque binaire : ok différencier les pièces : ok enregistrement pieces et masks : ok On  est biennnn

--- Analyse/main.py
+++ Analyse/main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import os
+import glob
 
 
 # création de l'image à partir d'un fichier
@@ -9,200 +11,71 @@ img = cv2.imread("./images/startImage.jpg")
 
 cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-if not cv2.cuda.getCudaEnabledDeviceCount():
-    print("Aucun GPU compatible CUDA trouvé. Utilisez la version CPU d'OpenCV.")
-    exit(0)
-
-gpu_image = cv2.cuda_GpuMat()
-gpu_image.upload(img)
-
-
-def Test_1(img):
-    print("Lancement du test 1 : ...")
-    # on applique un filtre passe-bas pour lisser l'image
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    # on convertit l'image en niveaux de gris
-    img_grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    # Calcule l'histogramme des couleurs
-    hist = cv2.calcHist([img_grey], [0], None, [256], [0, 256])
-
-    # Trouve l'intensité de gris qui a la fréquence la plus élevée dans l'histogramme
-    dominant_color = np.argmax(hist)
-
-    print("dominant_color : ", dominant_color)
-
-    tolerance = 12
-
-    # on parcourt l'image
-    for i in range(img_grey.shape[0]):
-        for j in range(img_grey.shape[1]):
-            # si la couleur est trop proche de la couleur dominante
-            # on la considère comme étant la couleur dominante
-            if abs(img_grey[i, j] - dominant_color) < tolerance:
-                # on chnage sur img la couleur de l'image
-                img_grey[i, j] = 255
-                # on change sur img la couleur de l'image
-            else:
-                img_grey[i, j] = 0
-
-    # on affiche l'image
-    cv2.imshow("Step2", img_grey)
-    cv2.waitKey(0)
-
-    # on affiche l'image
-    cv2.imshow("ImageFinale", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+list_widePieces = []
+list_widePieces_mask = []
+list_contours_sorted = []
+list_cutPieces = []
+list_cutPieces_mask = []
 
 
-def Test_2(img):
-    # Appliquer un filtre moyen
-    kernel_size = 15
-    mean_kernel = np.ones((kernel_size, kernel_size), np.float32) / (kernel_size**2)
-    img_mean = cv2.filter2D(img, -1, mean_kernel)
-
-    # Appliquer un filtre gaussien
-    kernel_size = 5
-    sigma = 1
-    img_gaussian = cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma)
-
-    img = cv2.bitwise_or(img_mean, img_gaussian)
-
-    img_grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    hist = cv2.calcHist([img_grey], [0], None, [256], [0, 256])
-
-    # Trouve l'intensité de gris qui a la fréquence la plus élevée dans l'histogramme
-    dominant_color = np.argmax(hist)
-
-    print("dominant_color : ", dominant_color)
-
-    tolerance = 12
-
-    # on parcourt l'image
-    for i in range(img_grey.shape[0]):
-        for j in range(img_grey.shape[1]):
-            # si la couleur est trop proche de la couleur dominante
-            # on la considère comme étant la couleur dominante
-            if abs(img_grey[i, j] - dominant_color) < tolerance:
-                # on chnage sur img la couleur de l'image
-                img_grey[i, j] = 255
-                # on change sur img la couleur de l'image
-            else:
-                img_grey[i, j] = 0
-
-    # on affiche l'image
-    cv2.imshow("Step2", img_grey)
-    cv2.waitKey(0)
-
-    kernel = np.ones((6, 6), np.uint8)
-
-    # on applique une fermeture
-    closing = cv2.morphologyEx(img_grey, cv2.MORPH_CLOSE, kernel)
-
-    # on applique une ouverture
-    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
-
-    # on applique un ET logique entre les deux images pour ne garder que les pixels qui sont à 1 dans les deux images
-    img_grey = cv2.bitwise_and(img_grey, opening)
-
-    cv2.imshow("Image_après_ET", img_grey)
-    cv2.waitKey(0)
-    kernel = np.ones((2, 2), np.uint8)
-    img_grey = cv2.morphologyEx(img_grey, cv2.MORPH_OPEN, kernel)
-
-    cv2.imshow("Image_après_fermeture", img_grey)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# vide les dossier
+def clearFolder(filename):
+    files = glob.glob(filename)
+    for f in files:
+        os.remove(f)
 
 
-def Test_3(img):
-    print("Lancement du test 3 : ...")
-    img = cv2.GaussianBlur(img, (5, 5), 3)
+clearFolder("./Mask/cutMask/*")
+clearFolder("./Mask/wideMask/*")
+clearFolder("./images/cutPieces/*")
+clearFolder("./images/widePieces/*")
 
-    # convertir l'image en niveaux de gris avec un seuillage adaptatif
-    gray = cv2.adaptiveThreshold(
-        cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
-        255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY_INV,
-        11,
-        2,
+
+def sort_contours(contours, img, tolerance_ratio=0.1):
+    # Tri des contours par ordre croissant en fonction de leur position en Y, puis en X
+    contours_sorted = sorted(
+        contours, key=lambda c: (cv2.boundingRect(c)[1], cv2.boundingRect(c)[0])
     )
 
-    cv2.imshow("Image_after_seuillage_auto", gray)
-    cv2.waitKey(0)
+    # Calcul de la tolérance verticale pour déterminer si deux contours sont sur la même ligne
+    tolerance = int(img.shape[0] * tolerance_ratio)
 
-    # appliquer une opération de fermeture pour combler les petits trous
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+    # Initialisation des lignes et de la première ligne avec le premier contour
+    lines = []
+    current_line = [contours_sorted[0]]
+    current_line_y = cv2.boundingRect(contours_sorted[0])[1]
 
-    cv2.imshow("Image_after_CLOSE", closed)
-    cv2.waitKey(0)
+    # Parcours des contours triés et regroupement par ligne
+    for contour in contours_sorted[1:]:
+        y = cv2.boundingRect(contour)[1]
+        if abs(y - current_line_y) <= tolerance:  # Si le contour est sur la même ligne
+            current_line.append(contour)
+        else:  # Si le contour est sur une nouvelle ligne
+            lines.append(current_line)
+            current_line = [contour]
+            current_line_y = y
 
-    # appliquer une opération d'ouverture pour combler les petits trous
-    opened = cv2.morphologyEx(closed, cv2.MORPH_OPEN, kernel)
+    # Ajout de la dernière ligne de contours
+    lines.append(current_line)
 
-    cv2.imshow("Image_after_OPEN", opened)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Tri des contours à l'intérieur de chaque ligne par ordre croissant en fonction de leur position en X
+    sorted_lines = [
+        sorted(line, key=lambda c: cv2.boundingRect(c)[0]) for line in lines
+    ]
 
+    # Fusion des contours triés par ligne pour obtenir la liste finale des contours triés par lignes et colonnes
+    sorted_contours = [contour for line in sorted_lines for contour in line]
 
-def Test_4(img):
-    img = cv2.GaussianBlur(img, (5, 5), 3)
-
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-
-    h, s, v = cv2.split(img)
-
-    ret_h, th_h = cv2.threshold(h, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    ret_s, th_s = cv2.threshold(s, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    ret_v, th_v = cv2.threshold(v, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # cv2.imshow("Image_after_seuillage_auto_h", th_h)
-    # cv2.imshow("Image_after_seuillage_auto_s", th_s)
-    # cv2.imshow("Image_after_seuillage_auto_v", th_v)
-    # cv2.waitKey(0)
-
-    cv2.bitwise_and(th_h, th_s, th_h)
-    cv2.bitwise_and(th_h, th_v, th_h)
-    cv2.imshow("Image_after_seuillage_auto_hsv", th_h)
-    cv2.waitKey(0)
-
-    kernel = np.ones((8, 8), np.uint8)
-    th = cv2.morphologyEx(th_h, cv2.MORPH_OPEN, kernel)
-
-    cv2.imshow("Image_after_seuillage_auto_hsv", th)
-    cv2.waitKey(0)
-
-    # cannay
-    edges = cv2.Canny(th, 100, 200)
-    cv2.imshow("Image_after_seuillage_auto_hsv", edges)
-    cv2.waitKey(0)
-
-    # dilate
-    kernel = np.ones((5, 5), np.uint8)
-    dilated = cv2.dilate(edges, kernel, iterations=1)
-    cv2.imshow("Image_after_seuillage_auto_hsv", dilated)
-    cv2.waitKey(0)
-
-    # find contours
-    contours, hierarchy = cv2.findContours(
-        dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-    )
-
-    # draw contours
-    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-
-    cv2.imshow("Image_after_seuillage_auto_hsv", img)
-    cv2.waitKey(0)
+    return sorted_contours
 
 
-def Test_5(img, tolerance):
+def Test_5(img, tolerance, tolH=0, tolS=0, tolV=0):
     img = cv2.medianBlur(img, 5)
-    # on convertit l'image en niveaux de gris
+
+    # enleve 5 pixels de chaque coté
+    img = img[5:-5, 5:-5]
+
+    # on convertit l'image en LAB
     img_lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
 
     # Calcule l'histogramme LAB
@@ -223,21 +96,115 @@ def Test_5(img, tolerance):
             # si la couleur est trop proche de la couleur dominante
             # on la considère comme étant la couleur dominante
             if (
-                abs(img_lab[i, j][1] - dominant_color_a) < tolerance
-                and abs(img_lab[i, j][2] - dominant_color_b) < tolerance
-                and abs(img_lab[i, j][0] - dominant_color_l) < tolerance
+                abs(img_lab[i, j][1] - dominant_color_a) < tolerance + tolH
+                and abs(img_lab[i, j][2] - dominant_color_b) < tolerance + tolS
+                and abs(img_lab[i, j][0] - dominant_color_l) < tolerance + tolV
             ):
                 img_lab[i, j] = 0
             else:
                 img_lab[i, j] = 255
-
     # fermeture
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (6, 6))
     img_lab = cv2.morphologyEx(img_lab, cv2.MORPH_CLOSE, kernel)
 
-    # on affiche l'image
-    cv2.imshow("Step2", img_lab)
-    cv2.waitKey(0)
+    # # ouverture
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (4, 4))
+    img_lab = cv2.morphologyEx(img_lab, cv2.MORPH_OPEN, kernel)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (6, 6))
+    img_lab = cv2.morphologyEx(img_lab, cv2.MORPH_CLOSE, kernel)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (8, 8))
+    img_lab = cv2.morphologyEx(img_lab, cv2.MORPH_OPEN, kernel)
+
+    # # dilate
+    kernel = np.ones((1, 1), np.uint8)
+    img_lab = cv2.dilate(img_lab, kernel, iterations=1)
+
+    kernel = np.ones((1, 1), np.uint8)
+    img_lab = cv2.erode(img_lab, kernel, iterations=1)
+
+    img_labL = img_lab[:, :, 0]
+
+    # # find contours
+    contours, hierarchy = cv2.findContours(
+        img_labL, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+
+    # Regarde si les contours ont été trouvés
+    if len(contours) > 0:
+        print("Nombre de contours trouvés = " + str(len(contours)))
+
+        contours = sort_contours(contours, img_labL)
+
+        # enleve le dernier contour car c'est le logo
+        contours = contours[:-1]
+
+        # Créer une image vierge de la même taille que l'original
+        img_pieces = np.zeros_like(img_labL)
+
+        img_pieces = cv2.cvtColor(img_pieces, cv2.COLOR_GRAY2RGB)
+
+        for i, cnt in enumerate(contours):
+            # Remplir la pièce avec une nuance de gris différente
+            new_col_R = np.random.randint(0, 255)
+            new_col_G = np.random.randint(0, 255)
+            new_col_B = np.random.randint(0, 255)
+            cv2.drawContours(
+                img_pieces, contours, i, (new_col_R, new_col_G, new_col_B), -1
+            )
+
+            x, y, w, h = cv2.boundingRect(cnt)
+            center_x, center_y = x + w // 2, y + h // 2
+
+            # Dessiner l'indice de la pièce au centre
+            cv2.putText(
+                img_pieces,
+                str(i),
+                (center_x, center_y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1.1,  # Taille de la police
+                (0, 0, 255),  # Couleur en BGR (rouge)
+                2,  # Epaisseur de la ligne
+            )
+
+        cv2.imshow("Pièces de puzzle en couleurs", img_pieces)
+        cv2.waitKey(0)
+
+        # avec chaque contour, on va créer un masque pour isoler la pièce
+        # et on va la stocker dans une liste
+
+        for i, cnt in enumerate(contours):
+            # Créer un masque vide
+            mask = np.zeros_like(img_labL)
+
+            # Dessiner le contour sur le masque
+            cv2.drawContours(mask, contours, i, 255, -1)
+
+            # Extraire la pièce de l'image originale
+            piece = cv2.bitwise_and(img_labL, img_labL, mask=mask)
+
+            list_widePieces_mask.append(piece)
+
+            x, y, w, h = cv2.boundingRect(cnt)
+
+            # Extraire la pièce de l'image originale
+            piece = piece[y : y + h, x : x + w]
+
+            list_cutPieces_mask.append(piece)
+
+        # on applique les masques sur l'image originale img
+        for i, piece in enumerate(list_widePieces_mask):
+            widePiece = cv2.bitwise_and(img, img, mask=piece)
+            list_widePieces.append(widePiece)
+
+            x, y, w, h = cv2.boundingRect(contours[i])
+
+            cutPiece = img[y : y + h, x : x + w]
+            list_cutPieces.append(cutPiece)
+
+    else:
+        print("No puzzle pieces found")
 
     return img_lab
 
@@ -255,7 +222,18 @@ def Test_5(img, tolerance):
 # Test_4(img)
 
 print("Test 5")
-cv2.cuda(Test_5(gpu_image, 20))
+Test_5(img, 12, 4, 5, 10)
+
+
+def saveInFile(list, path):
+    for i, piece in enumerate(list):
+        cv2.imwrite(path + str(i) + ".jpg", piece)
+
+
+saveInFile(list_cutPieces, "images/cutPieces/")
+saveInFile(list_widePieces, "images/widePieces/")
+saveInFile(list_cutPieces_mask, "Mask/cutMask/")
+saveInFile(list_widePieces_mask, "Mask/wideMask/")
 
 
 key = cv2.waitKey(0) & 0x0FF
